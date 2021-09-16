ee93aef955251c0a9666f50929a11a6f1fcd4db4
dmitry <di.im2015@yandex.ru>
2021-09-16 18:57:22 +0300

    Second seminar added

---
+++ Seminars/Seminar_2/src/edu/hse/cs/se/seminar2/Sample_0_Texts.java
@@ -0,0 +1,18 @@
+package edu.hse.cs.se.seminar2;
+
+import java.util.Date;
+
+/**
+ * This class is to show some text usages... Show step by step in debug mode...
+ */
+public class Sample_0_Texts {
+    public static void main(String[] args) {
+        System.out.println("Hello, World! - from JDK 16..."); // let's speak in English...
+        System.out.println("Здравствуйте, ребята... " + new Date()); // And let's speak in Russian...
+        char a = '\u005B';
+        char b = '\u005D';
+        System.out.println("a = " + a + ", b = " + b);
+        System.out.println("a = " + (int) a + ", b = " + (int) b);
+        System.out.println("a * b = " + a * b);
+    }
+}
